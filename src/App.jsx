e75b9364322b29f0f026ce5cb5751c8b75be8e36
Tamura <tamurafelipe@gmail.com>
2026-08-10 08:10:56 -0300

    feat: create home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import Home from "./pages/Home";
 function App() {
-  return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="h-screen w-screen overflow-x-auto scroll-smooth space-x-8">
+      <header>
+        <h1>Home</h1>
+      </header>
+
+      <main className="space-x-8">
+        <p>content</p>
+      </main>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
