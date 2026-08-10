26ce25ea037d7c594c6adbb904e6e4c37d61c9f8
Tamura <tamurafelipe@gmail.com>
2026-08-10 08:25:37 -0300

    feat: installed lucide-react  and add container component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,27 @@
+import { Container } from "../../components/Container";
+import { MenuIcon } from "lucide-react";
 const Home = () => {
   return (
-    <div className="h-screen w-screen overflow-x-auto scroll-smooth space-x-8">
+    <div className="h-screen w-screen overflow-x-auto scroll-smooth space-y-8">
       <header>
-        <h1>Home</h1>
+        <Container>
+          <div>
+            <img src="path/to/image.jpg" alt="Description" sizes="" srcset="" />
+          </div>
+          <nav className="lg:block hidden">
+            <ul>
+              <li>Home</li>
+              <li>Sobre</li>
+              <li>Serviços</li>
+              <li>Galeria</li>
+              <li>Contato</li>
+            </ul>
+          </nav>
+          <button>Agendar horário</button>
+          <div className="lg:hidden block">
+            <MenuIcon />
+          </div>
+        </Container>
       </header>
 
       <main className="space-x-8">
