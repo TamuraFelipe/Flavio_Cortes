e6139af4455b060a6d7961e1cc05099b1fe0f5f7
Tamura <tamurafelipe@gmail.com>
2026-09-01 11:19:18 -0300

    feat: add my github link on footer

---
+++ src/components/layout/Footer/index.jsx
@@ -0,0 +1,67 @@
+import Container from "../Container";
+import Logo from "../../ui/Logo";
+
+import { FaInstagram } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white">
+      <Container>
+        <div className="p-10 flex items-center justify-between">
+          <div className="flex gap-8">
+            <div className="flex flex-col gap-2">
+              <h5 className="font-semibold">Flavio Barbeiro</h5>
+              <div className="flex items-center gap-2">
+                <FaInstagram style={{ color: "#b08d57" }} />
+                <a
+                  href="https://www.instagram.com/tamuraflavio/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-gray-400 hover:text-brand transition-colors"
+                >
+                  @tamuraflavio
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h5 className="font-semibold">Dudan Barbershop</h5>
+              <div className="flex items-center gap-2">
+                <FaInstagram style={{ color: "#b08d57" }} />
+                <a
+                  href="https://www.instagram.com/dudanbarbearia/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-gray-400 hover:text-brand transition-colors"
+                >
+                  @dudanbarbearia
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <Logo />
+            <p className="text-gray-400 text-sm">
+              © 2023 Flavio Barbeiro. Todos os direitos reservados.
+            </p>
+          </div>
+          <div>
+            <div>
+              <p>
+                ❤️ Desenvolvido por{" "}
+                <a
+                  href="https://github.com/tamurafelipe"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-gray-400 hover:text-brand transition-colors"
+                >
+                  Felipe Tamura
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,11 +7,7 @@ import Galeria from "../../components/layout/Galeria";
 import Services2 from "../../components/layout/Services2";
 //import MidBanner from "../../components/layout/MidBanner";
 import Contato from "../../components/layout/Contato";
-
-import Container from "../../components/layout/Container";
-import Logo from "../../components/ui/Logo";
-
-import { FaInstagram } from "react-icons/fa";
+import Footer from "../../components/layout/Footer";
 
 const Home = () => {
   return (
@@ -27,47 +23,7 @@ const Home = () => {
         {/* <MidBanner /> */}
         <Contato />
       </main>
-
-      <footer className="bg-black text-white">
-        <Container>
-          <div className="p-10 flex items-center justify-between">
-            <Logo />
-            <p className="text-gray-400 text-sm">
-              © 2023 Flavio Barbeiro. Todos os direitos reservados.
-            </p>
-            <div className="flex gap-8">
-              <div>
-                <h5 className="font-semibold mb-2">Flavio Barbeiro</h5>
-                <div className="flex items-center gap-2">
-                  <FaInstagram style={{ color: "#b08d57" }} />
-                  <a
-                    href="https://www.instagram.com/tamuraflavio/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-gray-400 hover:text-brand transition-colors"
-                  >
-                    @tamuraflavio
-                  </a>
-                </div>
-              </div>
-              <div>
-                <h5 className="font-semibold mb-2">Dudan Barbershop</h5>
-                <div className="flex items-center gap-2">
-                  <FaInstagram style={{ color: "#b08d57" }} />
-                  <a
-                    href="https://www.instagram.com/dudanbarbearia/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-gray-400 hover:text-brand transition-colors"
-                  >
-                    @dudanbarbearia
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Container>
-      </footer>
+      <Footer />
     </div>
   );
 };
