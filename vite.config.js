55ffc31a8840afc41284f7230b15d0fedd098ad2
Tamura <tamurafelipe@gmail.com>
2026-09-02 10:05:44 -0300

    chore: add pwa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,27 @@ export default defineConfig({
       png: { quality: 80 },
       jpeg: { quality: 75 },
       webp: { quality: 80 },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg}"],
+      },
+      manifest: {
+        name: "Flavio Cortes",
+        short_name: "Flavio Cortes",
+        description:
+          "Flavio Cortes - Cortes de cabelo e barbearia em Araçatuba/SP",
+        theme_color: "#050505",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+        ],
+      },
     }),
   ],
   base: "./",
